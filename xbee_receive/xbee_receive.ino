70e87670844866d222f31b5e0cabd192367f60ca
wdesbiens <wdesb070@uottawa.ca>
2022-04-11 14:47:59 -0400

    changes to xbee_receive for demo purposes

--- xbee_receive/xbee_receive.ino
+++ xbee_receive/xbee_receive.ino
@@ -1,34 +1,14 @@
-/*
-  SerialPassthrough sketch
-
-  Some boards, like the Arduino 101, the MKR1000, Zero, or the Micro, have one
-  hardware serial port attached to Digital pins 0-1, and a separate USB serial
-  port attached to the IDE Serial Monitor. This means that the "serial
-  passthrough" which is possible with the Arduino UNO (commonly used to interact
-  with devices/shields that require configuration via serial AT commands) will
-  not work by default.
-
-  This sketch allows you to emulate the serial passthrough behaviour. Any text
-  you type in the IDE Serial monitor will be written out to the serial port on
-  Digital pins 0 and 1, and vice-versa.
-
-  On the 101, MKR1000, Zero, and Micro, "Serial" refers to the USB Serial port
-  attached to the Serial Monitor, and "Serial1" refers to the hardware serial
-  port attached to pins 0 and 1. This sketch will emulate Serial passthrough
-  using those two Serial ports on the boards mentioned above, but you can change
-  these names to connect any two serial ports on a board that has multiple ports.
-
-  created 23 May 2016
-  by Erik Nyquist
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/SerialPassthrough
-*/
-
 #include <SoftwareSerial.h>
 
 SoftwareSerial xbee(3, 2);
 
-int led = 5;
+int ledR1 = 5;
+int ledR2 = 6;
+int ledG1 = 7;
+int ledG2 = 8;
+int ledB1 = 10;
+int ledB2 = 12;
+
 int received = 0;
 
 void setup() {
@@ -37,35 +17,53 @@ void setup() {
 }
 
 void loop() {
-//  if (Serial.available()) {      // If anything comes in Serial (USB),
-//    xbee.write(Serial.read());   // read it and send it out Serial1 (pins 0 & 1)
-//  }al.write(xbee.read());   // read it and send it out Serial (USB)
-//  }
-
-
-
+ 
    if (xbee.available()) {
-
     received = xbee.read();
-
-    if (received == '0'){
-
-    Serial.println("Turning off LED");
-
-    digitalWrite(led, LOW);
-
+    Serial.println(received);
+ 
+    if (received >= 75) {
+      digitalWrite(ledB2, HIGH);
     }
-//
-//  if (xbee.available()) {     // If anything comes in Serial1 (pins 0 & 1)
-//    Seri
+    else {
+      digitalWrite(ledB2, LOW);
+    }
+    
+    if (received >= 60) {
+      digitalWrite(ledB1, HIGH);
+    }
+    else {
+      digitalWrite(ledB1, LOW);
+    }
+    
+    if (received >= 45) {
+      digitalWrite(ledG2, HIGH);
+    }
+    else {
+      digitalWrite(ledG2, LOW);
+    }
 
-    else if (received == '1'){
-
-    Serial.println("Turning on LED");
-
-    digitalWrite(led, HIGH);
-
+    if (received >= 30) {
+      digitalWrite(ledG1, HIGH);
+    }
+    else {
+      digitalWrite(ledG1, LOW);
+    }
+    
+    if (received >= 15) {
+      digitalWrite(ledR2, HIGH);
+    }
+    else {
+      digitalWrite(ledR2, LOW);
+    }
+    
+    if (received >= 0) {
+      digitalWrite(ledR1, HIGH);
+    }
+    else {
+      digitalWrite(ledR1, LOW);
     }
 
   }
+  
 }
